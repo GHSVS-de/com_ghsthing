955bc49aeaef1276c28240f9ca96d1aaf601f6b4
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-10-19 15:28:07 +0200

    #__ghsthing_frontpage to un/install SQLs

--- src/admin/sql/install.mysql.utf8.sql
+++ src/admin/sql/install.mysql.utf8.sql
@@ -45,3 +45,15 @@ CREATE TABLE IF NOT EXISTS `#__ghsthing`
   KEY `idx_language` (`language`),
   KEY `idx_alias` (`alias`(191))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `#__ghsthing_frontpage`
+--
+
+CREATE TABLE IF NOT EXISTS `#__ghsthing_frontpage` (
+  `ghsthing_id` int NOT NULL DEFAULT 0,
+  `ordering` int NOT NULL DEFAULT 0,
+  `featured_up` datetime,
+  `featured_down` datetime,
+  PRIMARY KEY (`ghsthing_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
--- src/admin/sql/uninstall.mysql.utf8.sql
+++ src/admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS `#__ghsthing`;
+DROP TABLE IF EXISTS `#__ghsthing_frontpage`;
