d3abb553754ea8d1514158407d2e165e0402baf4
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-10-20 23:59:50 +0200

    Update default.php

--- src/admin/tmpl/ghsthings/default.php
+++ src/admin/tmpl/ghsthings/default.php
@@ -9,6 +9,8 @@ use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
 
+$user = Factory::getUser();
+$userId = $user->id;
 ?>
 <h2>Welcome to My GhsThings!</h2>
 
@@ -22,14 +24,26 @@ use Joomla\CMS\Session\Session;
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($this->items as $i => $row) : ?>
+
+<?php
+foreach ($this->items as $i => $item) :
+	$canCreate  = $user->authorise('core.create', 'com_contact.category.' . $item->catid);
+	$canEdit    = $user->authorise('core.edit', 'com_contact.category.' . $item->catid);
+	$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || is_null($item->checked_out);
+	$canEditOwn = $user->authorise('core.edit.own', 'com_contact.category.' . $item->catid) && $item->created_by == $userId;
+	$canChange  = $user->authorise('core.edit.state', 'com_contact.category.' . $item->catid) && $canCheckin;
+	$item->cat_link = Route::_('index.php?option=com_categories&extension=com_contact&task=edit&type=other&id=' . $item->catid); ?>
             <tr>
-                <td><a href="/<?php echo Route::_('index.php?option=com_ghsthing&task=ghsthing.edit&id=' . $row->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($row->title); ?>">
-        <?php echo $this->escape($row->title); ?>
-    </a></td>
-                <td><?php echo $row->id; ?></td>
+		<td>
+			<a href="<?php echo Route::_('index.php?option=com_ghsthing&task=ghsthing.edit&id=' . $item->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>">
+				<?php echo $this->escape($item->title); ?>
+			</a>
+		</td>
+		<td>
+			<?php echo $item->id; ?>
+		</td>
             </tr>
-        <?php endforeach; ?>
+<?php endforeach; ?>
     </tbody>
 </table>
 
