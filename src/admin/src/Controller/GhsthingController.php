ad86e4994633a42db66bbe077b7a39b96505ad2b
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-10-21 20:33:48 +0200

    Update GhsthingController.php
    - Add VersionableControllerTrait.

--- src/admin/src/Controller/GhsthingController.php
+++ src/admin/src/Controller/GhsthingController.php
@@ -4,8 +4,10 @@ namespace GHSVS\Component\GhsThing\Administrator\Controller;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Versioning\VersionableControllerTrait;
 
 class GhsthingController extends FormController
 {
+	use VersionableControllerTrait;
 
 }
